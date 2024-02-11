5db5a75570036e6cfffdf877573440dadb0e4a4a
Keine Secrets <miethe.max@web.de>
2024-02-11 16:28:31 +0100

    Canceled keydown event

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
         return false;
     });
 });
+
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+        return false;
+    }
+});
