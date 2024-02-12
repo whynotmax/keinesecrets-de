88d7e25d9c11aa78c1bde614b78d4cf7e840784b
Keine Secrets <miethe.max@web.de>
2024-02-12 17:57:55 +0100

    Added <meta name="viewport" content="width=device-width, initial-scale=1">

---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    document.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function (e) {
+        disabledEvent(e);
+    }, false);
+    function disabledEvent(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else if (window.event) {
+            window.event.cancelBubble = true;
+        }
+        e.preventDefault();
+        return false;
+    }
+}
